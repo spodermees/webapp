18a0f67f07c923d75e007c8e460f4c66a56cd84f
Mees van der Waal <1052159@hr.nl>
2025-03-30 18:31:29 +0200

    test 1

--- sw.js
+++ sw.js
@@ -1,8 +1,40 @@
+const CACHE_NAME = "pwa-cache-v1";
+const ASSETS_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/manifest.json",
+    "/icon-192.png",
+    "/icon-512.png"
+];
+
+// Installeer de service worker en cache de bestanden
 self.addEventListener("install", (event) => {
-    console.log("Service Worker geïnstalleerd!");
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    console.log("Fetching:", event.request.url);
-  });
-  
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("Bestanden gecached");
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// Haal bestanden uit de cache of van het netwerk
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Verwijder oude caches bij update
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
+    );
+});
